4138daf898bdbeb79e947141f54e550ec7a28d7d
Luiza Semeniuk <luiza@Luizas-MacBook-Air.local>
2025-05-31 18:58:47 +0200

    corrplot

--- inwestycje/inwestycje.r
+++ inwestycje/inwestycje.r
@@ -1,6 +1,14 @@
 #imports:
 library(dplyr)
+library(corrplot)
 
-data <- read.csv("inwestycje/dane_koncowe.csv")
-summary(data)
+#import the dataset
+data_with_quarters <- read.csv("inwestycje/dane.csv")
+summary(data_with_quarters)
+
+data <- data_with_quarters[, !(names(data_with_quarters) %in% c( "X" , "X.1", "Year", "Quarter"))]
+rownames(data) <- NULL
+
+data_cor = cor(data)
+corrplot(data_cor)
 
